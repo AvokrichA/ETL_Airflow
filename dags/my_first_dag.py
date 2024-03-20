38548880c8ad98d5447359b54f5167a6ae57c7cb
Alexandra Chirkova <93881730+AvokrichA@users.noreply.github.com>
2024-03-20 17:35:58 +0300

    Update my_first_dag.py

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -6,10 +6,10 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
     return 'Привет, Александра!'
 
-dag = DAG('my_first_dag', description='Привет, Александра',
+dag = DAG('Привет, Александра', description='Привет, Александра',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+hello_operator = PythonOperator(task_id='Привет, Александра', python_callable=print_hello, dag=dag)
 
 hello_operator
