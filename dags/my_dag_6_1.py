0c25aa06191ab980c8a7a981cf7d3fa9551e5efa
Alexandra Chirkova <93881730+AvokrichA@users.noreply.github.com>
2024-03-26 00:04:05 +0300

    Update my_dag_6_1.py

--- dags/my_dag_6_1.py
+++ dags/my_dag_6_1.py
@@ -1,11 +1,12 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator, PythonOperator
+from airflow.operators.bash import BashOperator
 from datatime import datatime
-
-def gen():
-    return random.randrange(0, 10)
 
 dag =  DAG('my_dag_6_1', start_date=datatime(2023,1,1), schedule_interval='0 12 * * *',
           catchup=False)
 
-gen_operator = PythonOperator(task_id=gen, dag=dag)
+gen_bash = BashOperator(
+    task_id='gen', 
+    bash_command = 'echo $RANDOM'; exit 99;,
+    dag=dag,
+)
