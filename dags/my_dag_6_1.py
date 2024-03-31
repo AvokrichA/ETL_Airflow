0187abaded27d8175d5e4be842acb6edd6339c20
Alexandra Chirkova <93881730+AvokrichA@users.noreply.github.com>
2024-03-31 08:54:07 +0300

    Update my_dag_6_1.py

--- dags/my_dag_6_1.py
+++ dags/my_dag_6_1.py
@@ -6,7 +6,7 @@ dag =  DAG('my_dag_6_1', start_date=datatime(2023,1,1), schedule_interval='0 12 
           catchup=False)
 
 gen_bash = BashOperator(
-    task_id='gen', 
+    task_id= gen_bash, 
     bash_command = echo $RANDOM; exit 99;,
     dag=dag,
 )
