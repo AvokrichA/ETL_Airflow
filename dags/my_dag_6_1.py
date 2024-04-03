816b5db065789f9bae4ee069544f1170f420c24a
Alexandra Chirkova <93881730+AvokrichA@users.noreply.github.com>
2024-04-03 10:04:36 +0300

    Update my_dag_6_1.py

--- dags/my_dag_6_1.py
+++ dags/my_dag_6_1.py
@@ -4,8 +4,10 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datatime import datatime
 
-dag =  DAG('my_dag_6_1', start_date=datatime(2023,1,1), schedule_interval='0 12 * * *', start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-          catchup=False)
+dag =  DAG('my_dag_6_1',
+           schedule_interval='0 12 * * *', 
+           start_date = datatime(2023, 1, 1),
+           catchup=False)
 
 gen_bash = BashOperator(
     task_id= gen_bash, 
