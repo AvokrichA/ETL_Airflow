9391cef71850baf107f229d691d7fb88c02ff6fc
Alexandra Chirkova <93881730+AvokrichA@users.noreply.github.com>
2024-04-03 15:18:16 +0300

    Update my_dag_6_1.py

--- dags/my_dag_6_1.py
+++ dags/my_dag_6_1.py
@@ -6,7 +6,7 @@ from datatime import datatime
 
 dag =  DAG('my_dag_6_1',
            schedule_interval='0 12 * * *', 
-           start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),,
+           start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
            catchup=False)
 
 gen_bash = BashOperator(
