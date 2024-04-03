63d3c897a29dd31f0a31df6abe217b3cb38c03fa
Alexandra Chirkova <93881730+AvokrichA@users.noreply.github.com>
2024-04-03 07:50:56 +0300

    Update my_dag_6_1.py

--- dags/my_dag_6_1.py
+++ dags/my_dag_6_1.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datatime import datatime
 
-dag =  DAG('my_dag_6_1', start_date=datatime(2023,1,1), schedule_interval='0 12 * * *',
+dag =  DAG('my_dag_6_1', start_date=datatime(2023,1,1), schedule_interval='0 12 * * *', start_date=datatime( 2024, 1, 1),
           catchup=False)
 
 gen_bash = BashOperator(
